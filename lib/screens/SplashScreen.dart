f0003771662129998742439a24df8bd7cd290d12
user <dissanayakepiumi75@gmail.com>
2023-08-30 20:04:04 +0530

    add cover image

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -10,10 +10,16 @@ class _SplashScreenState extends State<SplashScreen>{
   @override
   Widget build(BuildContext context){
     return Material(
-      child: Column(
-        children: [
-          Image.asset("images/image2.jpg"),
-        ],
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/image2.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [],
+        ),
       ),
     );
   }
