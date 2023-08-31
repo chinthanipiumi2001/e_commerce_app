1a05b47054c0a646a7f7b29842ddf81907f7e429
user <dissanayakepiumi75@gmail.com>
2023-08-31 21:22:24 +0530

    add text

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -14,14 +14,31 @@ class _SplashScreenState extends State<SplashScreen>{
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
+          color: Colors.black,
           image: DecorationImage(
             image: AssetImage("images/image2.jpg"),
             fit: BoxFit.cover,
+            opacity: 0.4,
           ),
         ),
         child: Column(
-          children: [],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+                Icons.shopping_cart,
+                size: 200,
+                color:Color.fromARGB(255, 238, 80, 80),
+            ),
+            Text("Pandi Shopping center",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
+
       ),
     );
   }
