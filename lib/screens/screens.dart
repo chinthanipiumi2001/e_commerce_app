bfc61b1c85d1811a96c8140396b28201d8cbf1f9
user <dissanayakepiumi75@gmail.com>
2023-09-02 18:39:05 +0530

    update files

---
+++ lib/screens/screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Screens extends StatefulWidget{
+
+  @override
+  State<Screens> createState() => _ScreensState();
+}
+
+class _ScreensState extends State<Screens> {
+  int selectIndex =0;
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
